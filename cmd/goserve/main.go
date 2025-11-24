fb822d0c1d26a53244e6350a09c92f93c42b73b3
Rajesh Thapa <devrajeshthapa@gmail.com>
2025-11-24 12:30:57 +0545

    add handler

--- cmd/goserve/main.go
+++ cmd/goserve/main.go
@@ -6,30 +6,29 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/bokshi-gh/file-server/internal/handlers"
 )
 
 func main() {
+	rootDir := flag.String("root", ".", "Root directory to serve")
 	host := flag.String("host", "0.0.0.0", "Host to bind the server")
 	port := flag.String("port", "8080", "Port to run the server on")
-	root := flag.String("root", ".", "Root directory to serve")
 	verbose := flag.Bool("v", false, "Enable verbose logging")
-
-	addr := host + ":" + port
-
 	flag.Parse()
 
-	if _, err := os.Stat(*root); os.IsNotExist(err) {
-		log.Fatalf("Root directory does not exist: %s", *root)
+	if _, err := os.Stat(*rootDir); os.IsNotExist(err) {
+		log.Fatalf("Root directory does not exist: %s", *rootDir)
 	}
 
+	http.HandleFunc("/", handlers.ClientHandler(*rootDir, *verbose))
+	addr := *host + ":" + *port
 	if *verbose {
-		fmt.Printf("Serving root directory: %s\n", *root)
+		log.Printf("Serving root: %s", *rootDir)
+		log.Printf("Listening on http://%s", addr)
+	} else {
+		fmt.Printf("Server running on http://%s\n", addr)
 	}
-
-	http.HandleFunc("/", clientHandler)
-	log.Printf("HTTP server runnign on %s:%s", *host, *port)
-	err := http.ListenAndServe(addr, nil)
-	if err != nil {
-		log.Fatalf("HTTP server failed: %v", err)
-	}
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
+
---
+++ internal/handlers/client.go
@@ -0,0 +1,126 @@
+package handlers
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func ClientHandler(rootDir string, verbose bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		requestedPath := r.URL.Path
+		if requestedPath == "" || requestedPath == "/" {
+			requestedPath = "/"
+		}
+		fullPath := filepath.Join(rootDir, filepath.Clean(requestedPath))
+
+		// Prevent directory traversal
+		absRoot, _ := filepath.Abs(rootDir)
+		absPath, _ := filepath.Abs(fullPath)
+		if len(absPath) < len(absRoot) || absPath[:len(absRoot)] != absRoot {
+			http.Error(w, "Invalid path", http.StatusForbidden)
+			return
+		}
+
+		info, err := os.Stat(fullPath)
+		if err != nil {
+			http.Error(w, "Not found", http.StatusNotFound)
+			return
+		}
+
+		if info.IsDir() {
+			// Try to serve index.html if it exists
+			indexPath := filepath.Join(fullPath, "index.html")
+			if _, err := os.Stat(indexPath); err == nil {
+				serveFile(indexPath, w)
+				if verbose {
+					log.Printf("Serving file: %s", indexPath)
+				}
+				return
+			}
+
+			// Otherwise, generate directory listing with links
+			if verbose {
+				log.Printf("Listing directory: %s", fullPath)
+			}
+
+			entries, err := os.ReadDir(fullPath)
+			if err != nil {
+				http.Error(w, "Failed to read directory", http.StatusInternalServerError)
+				return
+			}
+
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			fmt.Fprintf(w, "<html><head><meta charset=\"utf-8\"><title>Index of %s</title></head><body><h1>Index of %s</h1><ul>", requestedPath, requestedPath)
+			for _, e := range entries {
+				name := e.Name()
+				link := name
+				if e.IsDir() {
+					link += "/"
+				}
+				fmt.Fprintf(w, `<li><a href="%s">%s</a></li>`, link, link)
+			}
+			fmt.Fprint(w, "</ul></body></html>")
+			return
+		}
+
+		// File requested
+		if verbose {
+			log.Printf("Serving file: %s", fullPath)
+		}
+		serveFile(fullPath, w)
+	}
+}
+
+func serveFile(fullPath string, w http.ResponseWriter) {
+	/*
+		Since switch expression has default so mime => application/octet-stream will always be overwritten
+	*/
+	mime := "application/octet-stream"
+	ext := strings.ToLower(filepath.Ext(fullPath))
+	switch ext {
+	case ".txt":
+		mime = "text/plain"
+	case ".html":
+		mime = "text/html"
+	case ".css":
+		mime = "text/css"
+	case ".js":
+		mime = "application/javascript"
+	case ".png":
+		mime = "image/png"
+	case ".jpg", ".jpeg":
+		mime = "image/jpeg"
+	case ".gif":
+		mime = "image/gif"
+	case ".svg":
+		mime = "image/svg+xml"
+	case ".json":
+		mime = "application/json"
+	case ".pdf":
+		mime = "application/pdf"
+	case ".mp4":
+		mime = "video/mp4"
+	case ".mp3":
+		mime = "audio/mpeg"
+	case ".zip":
+		mime = "application/zip"
+	default:
+		mime = "text/plain"
+	}
+
+	w.Header().Set("Content-Type", mime)
+
+	f, err := os.Open(fullPath)
+	if err != nil {
+		http.Error(w, "Failed to open file", http.StatusNotFound)
+		return
+	}
+	defer f.Close()
+
+	io.Copy(w, f)
+}
